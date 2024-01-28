f28486a247a03f0061e4833d5e43dedf8dcb1c88
Paweł Łukasik <lukasik.pawel@gmail.com>
2024-01-28 23:30:58 +0100

    Chapter 3: More Matrix operation; float comparison

--- src/lib.rs
+++ src/lib.rs
@@ -260,7 +260,7 @@ impl PartialEq for Matrix {
         else {
             for i in 0..self.size {
                 for j in 0 .. self.size  {
-                    if self.at(i,j) != other.at(i,j) {
+                    if self.at(i,j) - other.at(i,j) > EPS {
                         return false;
                     }
                 }
@@ -432,6 +432,23 @@ fn cofactor(a: Matrix, row: usize, col: usize) -> f32 {
                                    [-1.0, 1.0, -1.0, 1.0]);
 
     minor(a, row, col) * cofactors.at(row, col)
+}
+
+fn inverse(a: Matrix) -> Matrix {
+    assert!(a.clone().is_invertible());
+
+    let mut values = [[0.0; 4]; 4];
+    let size = a.clone().size;
+    let determinant = determinant(a.clone());
+    for row in 0..size {
+        for col in 0..size {
+            let c = cofactor(a.clone(), row, col);
+
+            values[col][row] = c / determinant;
+        }
+    }
+
+    Matrix::new4x4(values[0], values[1], values[2], values[3])
 }
 
 fn append_string_or_new_line(c: f32, line_len: usize) -> (String, usize, bool) {
@@ -1116,5 +1133,79 @@ mod matrix {
 
         assert_eq!(determinant(a.clone()), 0.0);
         assert!(!a.is_invertible());
+    }
+
+    #[test]
+    /// Calculating rhe inverse of a matrix
+    fn calculating_a_inverse_of_a_matrix() {
+        let a = Matrix::new4x4(
+            [-5.0, 2.0, 6.0, -8.0],
+        [1.0, -5.0, 1.0, 8.0],
+        [7.0, 7.0, -6.0, -7.0],
+        [1.0, -3.0, 7.0, 4.0]);
+        let b = inverse(a.clone());
+        assert_eq!(determinant(a.clone()), 532.0);
+        assert_eq!(cofactor(a.clone(), 2, 3), -160.0);
+        assert_eq!(b.at(3,2), -160.0/532.0);
+        assert_eq!(cofactor(a.clone(), 3, 2), 105.0);
+        assert_eq!(b.at(2,3), 105.0/532.0);
+        assert_eq!(b, Matrix::new4x4(
+            [0.21805, 0.45113, 0.24060, -0.04511],
+            [-0.80827, -1.45677, -0.44361, 0.52068],
+            [-0.07895, -0.22368, -0.05263, 0.19737],
+            [-0.52256, -0.81391, -0.30075, 0.30639]
+        ));
+    }
+
+    #[test]
+    /// Calculating the inverse of another matrix
+    fn calculating_the_inverse_of_another_matrix() {
+        let a = Matrix::new4x4(
+            [8.0, -5.0, 9.0, 2.0],
+        [7.0, 5.0, 6.0, 1.0],
+        [-6.0, 0.0, 9.0, 6.0],
+        [-3.0, 0.0, -9.0, -4.0]);
+
+        assert_eq!(inverse(a), Matrix::new4x4(
+            [-0.15385, -0.15385, -0.28205, -0.53846],
+            [-0.07692, 0.12308, 0.02564, 0.03077],
+            [0.35897, 0.35897, 0.43590, 0.92308],
+            [-0.69231, -0.69231, -0.76923, -1.92308]));
+    }
+
+    #[test]
+    /// Calculating the inverse of a third matrix
+    fn calculating_the_inverse_of_a_third_matrix() {
+        let a = Matrix::new4x4(
+            [9.0, 3.0, 0.0, 9.0],
+            [-5.0, -2.0, -6.0, -3.0],
+            [-4.0, 9.0, 6.0, 4.0],
+            [-7.0, 6.0, 6.0, 2.0]);
+
+        assert_eq!(inverse(a), Matrix::new4x4(
+            [-0.04074, -0.07778, 0.14444, -0.22222],
+            [-0.07778, 0.03333, 0.36667, -0.33333],
+            [-0.02901, -0.14630, -0.10926, 0.12963],
+            [0.17778, 0.06667, -0.26667, 0.33333]))
+    }
+
+    #[test]
+    /// Multiplying a product by its inverse
+    fn multiplying_a_product_by_irs_inverse() {
+        let a = Matrix::new4x4(
+        [3.0, -9.0, 7.0, 3.0],
+        [3.0, -8.0, 2.0, -9.0],
+        [-4.0, 4.0, 4.0, 1.0],
+        [-6.0, 5.0, -1.0, 1.0]);
+
+        let b = Matrix::new4x4(
+        [8.0, 2.0, 2.0, 2.0],
+        [3.0, -1.0, 7.0, 0.0],
+        [7.0, 0.0, 5.0, 4.0],
+        [6.0, -2.0, 0.0, 5.0]);
+
+        let c = a.clone() * b.clone();
+
+        assert_eq!(c * inverse(b), a);
     }
 }
